878079f6d135fa6bfb2ec452a36d01237a7a06a2
Gulrez <egulrezalam@gmail.com>
2025-08-18 14:48:10 +0530

    Remove commented-out code for Message and Build models to improve code clarity

--- api/models.py
+++ api/models.py
@@ -1,37 +1,8 @@
-
-# from django.db import models
-
-# class Message(models.Model):
-#     name = models.CharField(max_length=100)
-#     text = models.TextField()
-
-#     def __str__(self):
-#         return f"{self.name}: {self.text[:30]}"
-
-# class Build(models.Model):
-#     build_id = models.CharField(max_length=50, unique=True)
-#     frame = models.CharField(max_length=100)
-#     propellers = models.CharField(max_length=100, blank=True, null=True)
-#     motors = models.CharField(max_length=100, blank=True, null=True)
-#     battery = models.CharField(max_length=100, blank=True, null=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-
-#     def __str__(self):
-#         return self.build_id
-
-
-
-
-
-
-
-
-
 from django.db import models
 import uuid
 from django.utils import timezone
 from django.core.exceptions import ValidationError
+
 
 class Message(models.Model):
     name = models.CharField(max_length=100)
@@ -40,6 +11,7 @@ class Message(models.Model):
 
     def __str__(self):
         return f"{self.name}: {self.text[:30]}"
+
 
 class Build(models.Model):
     name = models.CharField(max_length=100)
